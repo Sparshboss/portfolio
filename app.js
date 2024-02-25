848d51b831a1bd81ea547ec02aa6438e377c87b7
sparshboss <sharmasparsh27july@gmail.com>
2024-02-25 15:23:29 +0530

    frist commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+    strings: ["Web Developer","ETHICAL-HACKER","Software Developer","Digital Marketier"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
